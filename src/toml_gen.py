6aeba54d86b73904dab3fe3f6d3d8eb09c9d6070
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-08-17 13:08:11 -0300

    melhorando ordem

--- src/toml_gen.py
+++ src/toml_gen.py
@@ -43,9 +43,11 @@ classifiers = [
 ]
 license = "GPL-3.0-only WITH Classpath-Exception-2.0 OR BSD-3-Clause"
 license-files = ["LICENSE"]
-keywords = ["writing", "translate"]
+keywords = ["library", "management", "organizer"]
 dependencies = [
-    "PyQt5"
+    "PyQt5",
+    "PyPDF2",
+    "scholarly"
 ]
 
 [project.urls]
